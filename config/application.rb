da78e9c722ed7601f0baa7c631b181584253257f
Jonathan Berk <jdsberk@gmail.com>
2015-06-25 16:55:53 -0700

    WIP diamonds for social links

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module Berkjon
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Include Bower components in compiled assets
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
   end
 end
