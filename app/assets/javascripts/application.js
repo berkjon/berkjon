e131859a5dff3662782920d2ad5247337b8b079c
Jonathan Berk <jdsberk@gmail.com>
2015-06-24 19:10:01 -0700

    Full screen background image with parallax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,15 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+  // init controller
+  var controller = new ScrollMagic.Controller();
+
+    $(function () { // wait for document ready
+    // build scene
+    var scene = new ScrollMagic.Scene({triggerElement: "nav"})
+            .setPin("nav")
+            // .addIndicators({name: "2 (duration: 0)"}) // add indicators (requires plugin)
+            .addTo(controller);
+  });
